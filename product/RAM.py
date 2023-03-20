d6e955266ec43de2c1a4ac2816ed6d7746e16e6d
AgogFox <48616241+AgogFox@users.noreply.github.com>
2023-03-21 00:01:44 +0700

    refactor: fix indentation and manage to folders

--- product/RAM.py
+++ product/RAM.py
@@ -1,6 +1,5 @@
 from ui.Product import Product
 
-
 class RAM(Product):
     def __init__(self, model, specification, warranty, price):
         super().__init__(model, specification, warranty, price)
