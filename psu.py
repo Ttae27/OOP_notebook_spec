c73041d99c113d17dbaa4e69e3a1255e83c3b7c9
Withwws <mondoi2003@gmail.com>
2023-03-20 22:43:29 +0700

    <build>:initiate class3

---
+++ psu.py
@@ -0,0 +1,15 @@
+class PSU:
+    def __init__(self, model, specification, warranty, price):
+        self._model = model
+        self._specification = specification
+        self._warranty = warranty
+        self._price = price
+        
+    def add(self):
+        pass
+    
+    def remove(self):
+        pass
+    
+    def update(self):
+        pass
