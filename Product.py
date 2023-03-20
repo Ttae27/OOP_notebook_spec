6f7751ed9cab791149b7ed32ccaad7d8a2559d88
Ttae27 <ponghong.2744@gmail.com>
2023-03-20 22:51:49 +0700

    <build>: initiate class2

---
+++ Product.py
@@ -0,0 +1,15 @@
+class Product():
+    def __init__(self, price, model, specification, warranty):
+        self.price = price
+        self.model = model
+        self.specification = specification
+        self.warranty = warranty
+
+    def add():
+        pass
+
+    def remove():
+        pass
+
+    def update():
+        pass
