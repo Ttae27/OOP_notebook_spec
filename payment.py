f98bf6febe8e6cf96b935b32175bf6d39241ed9a
Withwws <mondoi2003@gmail.com>
2023-03-29 00:08:21 +0700

    <build>:payment feature

---
+++ payment.py
@@ -0,0 +1,34 @@
+class Payment:
+    def __init__(self, build):
+        self.amount = build.totalPrice
+
+    def paid_by_credit_card(self):
+        # code to process credit card payment goes here
+        card_number = input("card_number:")
+        self.use_coupon()
+        print(f"Paid ${self.amount} by credit card ({card_number})")
+
+    def paid_by_cashtransfer(self):
+        # code to process card transaction payment goes here
+        cash = int(input("cash:"))
+        self.use_coupon()
+        if cash < self.amount:
+            print("Not enough cash provided.")
+        else:
+            change = cash - self.amount
+            print(f"Paid ${self.amount} by cash tranfer. Balance: ${change:.2f}")
+
+    def use_coupon(self):
+        coupon_check = input("Coupon? y/n : ")
+        if coupon_check == "y":
+            coupon_code = input("coupon: ")
+            if coupon_code == "sugardaddy":
+                discount = self.amount * 0.1
+                self.amount -= discount
+                print(f"Discount applied: ${discount:.2f}. New total: ${self.amount:.2f}")
+            else:
+                print("Invalid coupon code.")
+        else:
+            print("No Discount applied")
+
+            
