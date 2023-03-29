dc020a97ddc59a46c59d4f19e10b32bd4bebf88c
Pearwa-likeLion <fittokung@gmail.com>
2023-03-29 15:49:46 +0700

    Len card Number

--- user/account.py
+++ user/account.py
@@ -1,10 +1,3 @@
-from user.information import Information
-
 class Account:
-    def __init__(self,id,username,password):
-        self.__id = id
-        self.__username = username
-        self.__password = password
-
-    def method(self):
+    def __init__(self):
         pass
--- user/admin.py
+++
@@ -1,6 +0,0 @@
-class Admin:
-    def __init__(self,delivery_address):
-        self.__delivery_address = delivery_address
-
-    def method(self):
-        pass
