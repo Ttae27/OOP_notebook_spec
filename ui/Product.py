eb5ef46caf816481a7bdd36ba77cf04815199153
AgogFox <48616241+AgogFox@users.noreply.github.com>
2023-04-17 23:05:06 +0700

    create product class pseudo

---
+++ ui/Product.py
@@ -0,0 +1,21 @@
+class Product():
+    def __init__(self) -> None:
+        pass
+
+    def get(filter =  None):
+        products = 0 #* get from database
+        return products
+
+    def delete(product_id):
+        #*delete item of that id
+        pass #* return status code
+
+    def modify(product_id, other_stuff):
+        #*update product accordingly
+        #*if NOT exist give out error
+        pass #*return status code
+
+    def add(product_id, other_stuff):
+        #*add product
+        #* if exist give out error
+        pass #*return status code
