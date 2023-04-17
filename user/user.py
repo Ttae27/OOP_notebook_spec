e886f272d0cc518cca948e3e42ba2fc7dcbdd93c
AgogFox <48616241+AgogFox@users.noreply.github.com>
2023-04-17 23:04:42 +0700

    create user class pseudo

---
+++ user/user.py
@@ -0,0 +1,21 @@
+class User():
+    def __init__(self) -> None:
+        pass
+
+    def get(id = some_number):
+        user = 0 #* get from database
+        return user
+
+    def delete(user_id):
+        #*delete user of that id
+        pass #* return status code
+
+    def modify(user_id, other_stuff):
+        #*update user accordingly
+        #*if NOT exist give out error
+        pass #*return status code
+
+    def add(user_id, other_stuff): #?user_id maybe add automatically by database
+        #*add user
+        #* if exist give out error
+        pass #*return status code
