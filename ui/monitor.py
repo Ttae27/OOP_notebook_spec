945dc998aec30fbe041d8f629fe2eea178e5630f
Withwws <mondoi2003@gmail.com>
2023-04-19 17:03:33 +0700

    <edit>:init method

--- ui/monitor.py
+++ ui/monitor.py
@@ -1,14 +1,26 @@
-class HDD:
-    def __init__(self, id, model, form_factor, capacity, interface, rpm, buffer, warranty, brand_name, full_name, price, thumbnail_url):
-        self.__id = id
-        self.__model = model
-        self.__form_factor = form_factor
-        self.__capacity = capacity
-        self.__interface = interface
-        self.__rpm = rpm
-        self.__buffer = buffer
-        self.__warranty = warranty
-        self.__brand_name = brand_name
-        self.__full_name = full_name
-        self.__price = price
-        self.__thumbnail_url = thumbnail_url
+class Monitor:
+    def __init__(self, id, series, model, size, hdr_detail, reso, ratio, refresh_rate, response, bright, speaker, hdmi, displayport, mini_displayport, dvi, vga, tech_feature, flicker_free, warranty, brand_name, full_name, price, thumbnail_url, cont):
+        self.id = id
+        self.series = series
+        self.model = model
+        self.size = size
+        self.hdr_detail = hdr_detail
+        self.reso = reso
+        self.ratio = ratio
+        self.refresh_rate = refresh_rate
+        self.response = response
+        self.bright = bright
+        self.speaker = speaker
+        self.hdmi = hdmi
+        self.displayport = displayport
+        self.mini_displayport = mini_displayport
+        self.dvi = dvi
+        self.vga = vga
+        self.tech_feature = tech_feature
+        self.flicker_free = flicker_free
+        self.warranty = warranty
+        self.brand_name = brand_name
+        self.full_name = full_name
+        self.price = price
+        self.thumbnail_url = thumbnail_url
+        self.cont = cont
