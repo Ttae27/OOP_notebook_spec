06d9ecacbb90df8d7561743c6a2648e287c6a055
Ttae27 <ponghong.2744@gmail.com>
2023-04-20 21:00:23 +0700

    <added>: database.db

--- ui/Product.py
+++ ui/Product.py
@@ -1,11 +1,25 @@
+import sys
+
+# setting path
+sys.path.append('../oop_notebook_spec')
 import sqlite3
 from data import *
+from data_class.case import Case
+from data_class.cooling import Cooling
+from data_class.cpu import CPU
+from data_class.gpu import GPU
+from data_class.hdd import HDD
+from data_class.monitor import Monitor
+from data_class.motherboard import Motherboard
+from data_class.psu import PSU
+from data_class.ram import RAM
+from data_class.ssd import SSD
 
 class Product():
     def __init__(self) -> None:
         pass
 
-    def get(cat: str, filter :dict = None):
+    def get(self, cat: str, filter :dict = None):
         def dict_factory(cursor, row):#convert list of tuple to list of dict
             d = {}
             for idx, col in enumerate(cursor.description):
@@ -35,26 +49,54 @@ class Product():
         conn.close()
         return products
 
-    def delete_product(cat, product_id):
+    def delete_product(self, cat, product_id):
         list_cat = get_list(cat)
-        for lst in list_cat:
-            if lst.id == product_id:
-                list_cat.remove(lst)
+        for product in list_cat:
+            if product.id == product_id:
+                list_cat.remove(product)
                 return {'status': 'Successfully delete product ' + str(product_id)}
         return {'status': 'Failed delete product'}
 
-    def modify_product(cat, product_id, product_key, product_value):
+    def modify_product(self, cat, product_id, product_key, product_value):
         list_cat = get_list(cat)
-        for lst in list_cat:
-            if lst.id == product_id:
-                lst.product_key = product_value
+        for product in list_cat:
+            if product.id == product_id:
+                product.product_key = product_value
                 return {'status': 'Successfully modified product ' + str(product_id)}
         return {'status': 'Failed modified product'}
 
-    def add_product(cat, product):
+    def add_product(self, cat, product):
         list_cat = get_list(cat)
-        for lst in list_cat:
+        for product in list_cat:
+            if product.id == product[id]:
+                return {'status': 'Failed to added product'}
+        list_cat.append(self.convert_to_class(cat, product))
+        return {'status': 'Successfully added product'}
 
+    def convert_to_class(cat, product):
+        if cat == "case":
+            return Case(**product)
+        elif cat == "cooling":
+            return Cooling(**product)
+        elif cat == "cpu":
+            return CPU(**product)
+        elif cat == "gpu":
+            return GPU(**product)
+        elif cat == "hdd":
+            return HDD(**product)
+        elif cat == "monitor":
+            return Monitor(**product)
+        elif cat == "motherboard":
+            return Motherboard(**product)
+        elif cat == "psu":
+            return PSU(**product)
+        elif cat == "ram":
+            return RAM(**product)
+        elif cat == "ssd":
+            return SSD(**product)
+        else:
+            return None
 
 #!test
 #print(Product.get('cpu', {'brand_name': ['INTEL'], 'series': ['Core i3', 'Ryzen 7']}))
+print(Product.delete_product('cpu', 6))
