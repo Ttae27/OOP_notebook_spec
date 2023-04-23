8df8bf9e662953347bf220f5d1bb4a93137da46d
Ttae27 <ponghong.2744@gmail.com>
2023-04-23 16:27:15 +0700

    <added>: api get_spec in main

--- ui/Catalog.py
+++ ui/Catalog.py
@@ -12,7 +12,11 @@ class Catalog():
         return lst
 
     def get_item(self, cat :str, product_id :str):
-        return self.__product_class.get_product(cat, {'id': [product_id]})
+        products = self.__product_class.get_product(cat, {'id': [product_id]})
+        lst = []
+        for product in products:
+            lst.append(vars(product))
+        return lst
 
     def search(self, cat, full_name):
         products = self.__product_class.get_product(cat, {'full_name': full_name})
