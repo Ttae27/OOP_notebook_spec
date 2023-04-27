879e7e19b516565b1e50035e5894558e38cc5aff
Ttae27 <ponghong.2744@gmail.com>
2023-04-27 21:50:35 +0700

    <fixed>: method in user, account

--- ui/payment.py
+++ ui/payment.py
@@ -1,38 +1,13 @@
+from datetime import datetime
 class Payment:
-    def __init__(self, build):
-        self.amount = build.totalPrice
-        
-    def paid_by_credit_card(self):
-        # code to process credit card payment goes here
-        card_number = input("card_number:")
-        if len(card_number) != 13:
-            print("Invalid Card Number, please recheck your Card Number")
-        else:
-            print(f"Paid ${self.amount} by credit card ({card_number})")
-
-    def paid_by_cashtransfer(self):
-        # code to process card transaction payment goes here
-        cash = int(input("cash:"))
-        if cash < self.amount:
-            print("Not enough cash provided.")
-        else:
-            change = cash - self.amount
-            print(f"Paid ${self.amount} by cash tranfer. Balance: ${change:.2f}")
-
-    def use_coupon(self):
-        coupon_check = input("Coupon? y/n : ")
-        if coupon_check == "y":
-            coupon_code = input("coupon: ")
-            if coupon_code == "sugardaddy":
-                discount = self.amount * 0.1
-                self.amount -= discount
-                print(f"Discount applied: ${discount:.2f}. New total: ${self.amount:.2f}")
-            else:
-                print("Invalid coupon code.")
-        else:
-            print("No Discount applied")
-            
-    def pay_status(self):
-        pass
-
-            
+    def __init__(self,amount,transaction_id,payment_status):
+        self.__amount = amount
+        self.__created_on = datetime.now()
+        self.__transaction_id = transaction_id
+        self.__status = payment_status
+    
+    @property    
+    def data_payment(self):
+        date = self.__created_on.strftime("%d/%m/%Y")
+        time = self.__created_on.strftime('%H:%M:%S')
+        return {'Transaction id': self.__transaction_id,'Date': date,'Time': time,'Amount': self.__amount,'Status': self.__status}
--- user/account.py
+++ user/account.py
@@ -1,39 +1,48 @@
-from data import *
+from data import get_user, add_user, update_user_data, del_user
 
 class Account:
     def __init__(self):
-        self.__allaccount = []
-        self.__loginstatus = "Success"
-    
-    def sign_in(self, username: str, password: str):
         self.__allaccount = get_user()
-        for account in self._allaccount:
-            if account.username == username and account.password == password:
-                return {"Status": "log in successfully"}
-        return {"Status" : "Try again"}
+        self.__current_user = None
 
-    def get_allaccount(self,account):
+    def sign_up(self, user_data: tuple) -> dict:
+        if add_user(user_data):
+            self.__allaccount = get_user()
+            return {'Notify': 'Successfully sign up'}
+        else:
+            return {'Notify': 'This username has been used'}
+    
+    def sign_in(self, username: str, password: str) -> dict:
         self.__allaccount = get_user()
-    
-    def login(self,username,password):
-        for i in range(len(self.__allaccount)):
-            if self.__allaccount[i].username == username and password == self.__allaccount[i].password:
-                self.__loginstatus = "Success"
-                break
-            else:
-                self.__loginstatus = "Failed"
-        return self.__loginstatus
-    
-    @property
-    def loginstatus(self):
-        return self.__loginstatus
-    
+        for account in self.__allaccount:
+            if account.username == username:
+                if account.auth(username, password):
+                    self.__current_user = account
+                    return {"Status": "log in successfully"}
+
+                else:
+                    return {"Status": "Incorrect password. Please try again."}
+
+        return {"Status": "User does not exist. Do you want to sign up?"}
+
+    def update_user(self, user_id: int, type: str, new_data) -> dict:
+        if update_user_data(user_id, type, new_data):
+            self.__allaccount = get_user()
+            return {"Status": "update data successfully"}
+
+        return {"Status": "Fail to update data"}
+
+    def delete_user(self, user_id: int):
+        if del_user(user_id):
+            self.__allaccount = get_user()
+            return {"Status": "delete successfully"}
+
+        return {"Status": "Fail to delete user"}
+
     @property
     def allaccount(self):
         return self.__allaccount
-    
-    # @property
-    # def find_account(self):
-    #     return self.__allaccount[0].username
 
-# acc=[["Pearwa","LOLO","aHah"],["mon","Kaka","yaga"],["bo","jaja","papa"],["tae","kim","lala"]]
+    @property
+    def current_user(self):
+        return self.__current_user
